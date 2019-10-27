2e69dc8e254a5dbfa236dc6dbd1b97916e59b311
Guayo Mena <guayo.mena@gmail.com>
2019-10-26 20:19:14 -0600

    Draft 1

---
+++ components/Iframe.js
@@ -0,0 +1,3 @@
+import React from "react"
+
+export default ({src}) => <iframe src={src} width="700px" height="400px" style={{border: `0`}}/>
--- snippets/create-pages.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(api) {
-  api.createPages(({ createPage }) => {
-    createPage({
-      path: "/my-page",
-      component: "./src/templates/MyPage.vue"
-    });
-  });
-};
