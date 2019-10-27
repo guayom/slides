22a4a8c8bac7e2bf12b704d59f56714f9497551e
Guayo Mena <guayo.mena@gmail.com>
2019-10-27 09:06:31 -0600

    Upgrade to mdx-deck 3

--- components/Iframe.js
+++ components/Iframe.js
@@ -1,3 +1,3 @@
 import React from "react"
 
-export default ({src}) => <iframe src={src} width="700px" height="400px" style={{border: `0`}}/>
+export default ({src}) => <iframe title="I frame" src={src} width="700px" height="400px" style={{border: `0`}}/>
---
+++ themes/guayo.js
@@ -0,0 +1,45 @@
+import theme from 'mdx-deck/themes'
+
+export default {
+  ...theme,
+  h1: {
+    textTransform: 'uppercase',
+    letterSpacing: '0.1em'
+  },
+  blockquote: {
+    fontStyle: 'italic'
+  }
+}
+
+// export default {
+//   font: '"Comic Sans MS", cursive, sans-serif',
+//   colors: {
+//     text: "#fff",
+//     background: "#f00",
+//     blue: "blue",
+//     link: "blue",
+//     pre: "blue",
+//     preBackground: "#f00",
+//     code: "blue"
+//   },
+//   heading: {
+//     textTransform: "uppercase",
+//     letterSpacing: "0.1em",
+//     fontWeight: 600
+//   },
+//   quote: {
+//     fontWeight: 600
+//   }
+// };
+
+// export default {
+//   styles: {
+//     h1: {
+//       textTransform: "uppercase",
+//       letterSpacing: "0.1em"
+//     },
+//     blockquote: {
+//       fontStyle: "italic"
+//     }
+//   }
+// };
