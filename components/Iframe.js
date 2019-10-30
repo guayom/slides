b377f5f6ca5dc71637b6a42bdb62c72ed87e25f1
Guayo Mena <guayo.mena@gmail.com>
2019-10-30 12:34:40 -0600

    Add loader and background to iframe component

--- components/Iframe.js
+++ components/Iframe.js
@@ -1,3 +1,24 @@
 import React from "react"
 
-export default ({src}) => <iframe title="I frame" src={src} width="700px" height="400px" style={{border: `0`}}/>
+export default ({ src }) => (
+  <div
+    style={{
+      boxShadow: `3px 3px 6px -5px rgba(0,0,0,0.75)`,
+      border: `solid 1px`,
+      background: `url(https://res.cloudinary.com/guayo/image/upload/v1572459895/slides/loader.gif) #fff center center no-repeat`
+    }}
+  >
+    <iframe
+      title="I frame"
+      src={src}
+      width="700px"
+      height="400px"
+      style={{
+        margin: 0,
+        padding: 0,
+        border: `none`,
+        display: `block`
+      }}
+    />
+  </div>
+);
