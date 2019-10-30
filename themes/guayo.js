46423ae9c2ea37d7177832d4f12f8d09880396a9
Guayo Mena <guayo.mena@gmail.com>
2019-10-30 10:43:55 -0600

    Add guayo Theme

--- themes/guayo.js
+++ themes/guayo.js
@@ -1,45 +1,49 @@
-import theme from 'mdx-deck/themes'
+const colors = {
+  CitrineWhite: "#FAFAD6",
+  BlueZodiac: "#12224C",
+  Jaffa: "#F67242"
+};
+
+const googleFont =
+  "https://fonts.googleapis.com/css?family=Archivo:400,500,700&display=swap";
 
 export default {
-  ...theme,
-  h1: {
-    textTransform: 'uppercase',
-    letterSpacing: '0.1em'
+  googleFont,
+  fonts: {
+    body: "'Archivo', sans-serif",
+    monospace: '"Roboto Mono", monospace'
   },
-  blockquote: {
-    fontStyle: 'italic'
+  fontSizes: [25, 40, 50, 20, 24, 32, 48, 64],
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700
+  },
+  colors: {
+    text: colors.BlueZodiac,
+    background: colors.CitrineWhite,
+    primary: colors.Jaffa
+  },
+  styles: {
+    root: {
+      textAlign: "left",
+      fontSize: ["1.5em", "3em"]
+    },
+    h1: {
+      letterSpacing: "0em",
+      color: colors.Jaffa,
+      margin: 2,
+      fontSize: "80px",
+      textAlign: "left"
+    },
+    h2: {
+      color: colors.Jaffa
+    },
+    p: {
+      fontSize: 0
+    },
+    li: {
+      fontSize: 0
+    }
   }
-}
-
-// export default {
-//   font: '"Comic Sans MS", cursive, sans-serif',
-//   colors: {
-//     text: "#fff",
-//     background: "#f00",
-//     blue: "blue",
-//     link: "blue",
-//     pre: "blue",
-//     preBackground: "#f00",
-//     code: "blue"
-//   },
-//   heading: {
-//     textTransform: "uppercase",
-//     letterSpacing: "0.1em",
-//     fontWeight: 600
-//   },
-//   quote: {
-//     fontWeight: 600
-//   }
-// };
-
-// export default {
-//   styles: {
-//     h1: {
-//       textTransform: "uppercase",
-//       letterSpacing: "0.1em"
-//     },
-//     blockquote: {
-//       fontStyle: "italic"
-//     }
-//   }
-// };
+};
